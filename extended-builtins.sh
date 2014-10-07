9e3e1247ddbf72173e9146e8f595d15c2f73e69b
Lars Winderling <lwinderling@performance-advertising.de>
2014-10-07 09:46:03 +0200

    initial commit

---
+++ extended-builtins.sh
@@ -0,0 +1,50 @@
+function import-ns() {
+  if [ $# -eq 0 ]; then
+    echo "error in function 'ns': no arguments given"
+  else
+    file="$1"
+    namespace="$2"
+  fi
+
+  if [ "${namespace}" = '' ]; then
+    source "$file"
+  else
+    tmp_file="$(mktemp)"
+    namespace="${namespace}."
+    private_token='__'
+    replace_str=''
+    sed_args=''
+    set -- $(<"$file" grep -v '\<private;[[:space:]]\+\<function\>' | grep -o '\<function[[:space:]]\+\<[a-zA-Z_][a-zA-Z0-9_\-]*\>')
+
+    while (( $# )); do
+      if [ "$1" != 'function' -a "${1:0:2}" != '__' ]; then
+        sed_args="${sed_args};s/([^a-zA-Z0-9_])(\<${1}\>)([^a-zA-Z0-9_.#/-])/\1${namespace}\2\3/g"
+      fi
+      shift
+    done
+
+    set -- $(<"$file" grep '\<private;[[:space:]]\+\<function\>' | grep -o '\<function[[:space:]]\+\<[a-zA-Z_][a-zA-Z0-9_\-]*\>')
+
+    while (( $# )); do
+      if [ "$1" != 'function' -a "${1:0:2}" != '__' ]; then
+        sed_args="${sed_args};s/([^a-zA-Z0-9_.#:\"'/-])(\<${1}\>)([^a-zA-Z0-9_.#:\"'/-])/\1${private_token}\2\3/g"
+      fi
+      shift
+    done
+
+    if [ "$sed_args" != '' ]; then
+      source <(<"$file" sed -r "${sed_args}")
+    else
+      source "$file"
+    fi
+  fi
+}
+
+function private() { :; }
+
+function export-into-ns() {
+  while (( $# )); do
+    export "$1"
+    shift
+  done
+}
---
+++ helpers.sh
@@ -0,0 +1,85 @@
+source extended-builtins.sh
+
+function require-argument {
+  export-into-ns required_args="${required_args} ${1}"
+}
+
+function require-arguments() {
+  while (( "$#" )); do
+    require-argument "$1"
+    shift
+  done
+}
+
+private; function with-arguments() {
+  if [ $# -gt 1 ]; then
+    func="$1"
+    shift
+    $func "$@"
+  else
+    echo "error in function '${func}': no arguments given" >&2
+    exit 1
+  fi
+}
+
+private; function undash() {
+  with-arguments \
+     echo "${1}" | sed 's/^-\+//g'
+}
+
+private; function export-into-ns-cli-arg() {
+  while (( $# )); do
+    export-into-ns "\$(${*})"
+    shift
+  done
+}
+
+function parse-arguments() {
+  while (( "$#" )); do
+    arg="$1"
+    echo "${arg}" |  grep '^\-\-[a-z_]\+=[a-zA-Z_.0-9\-]\+$' > /dev/null
+    if [ $? -eq 0 ]; then
+      export-into-ns $(undash ${arg})
+    else
+      echo "${arg}" |  grep '^\-\-[a-z_]\+$' > /dev/null
+      if [ $? -eq 0 ]; then
+        export-into-ns $(undash ${arg}=1)
+      else
+        echo "error: could not eval argument '${arg}'. exiting..."
+        exit 1
+      fi
+    fi
+    shift
+  done
+}
+
+function check-for-absent-arguments() {
+  for arg in $required_args; do
+    [[ -z "$(eval echo \$$(echo ${arg}))" ]] &&
+      echo "error: argument '${arg}' was not set. exiting..." &&
+      exit 1
+  done
+}
+
+function ascii-ts() {
+  date +'%Y-%m-%d %H:%M:%S' "$@"
+}
+
+function ascii-date() {
+  date +'%Y-%m-%d' "$@"
+}
+
+function ascii-time() {
+  date +'%H:%M:%S' "$@"
+}
+
+function unix-ts() {
+  date +'%s' "$@"
+}
+
+# TODO: function date-diff, taking care of ascii/unix-ts conversion before subtraction etc.
+function diff-ts() {
+  lhs="$(date '%s' -d ${1})"
+  rhs="$(date '%s' -d ${2})"
+  echo "$(( lhs - rhs ))"
+}
