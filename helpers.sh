8d1feb87d89dcc7fe708ee1d0f8f18a098c6b959
Lars Winderling <lwinderling@performance-advertising.de>
2014-10-31 18:11:17 +0100

    improve argument parsing

--- helpers.sh
+++ helpers.sh
@@ -25,24 +25,62 @@ private; function undash() {
      echo "${1}" | sed 's/^-\+//g'
 }
 
-function parse-arguments() {
-  while (( "$#" )); do
-    arg="$1"
-    echo "${arg}" |  grep '^\-\-[a-z_]\+=[a-zA-Z_.0-9\-]\+$' > /dev/null
-    if [ $? -eq 0 ]; then
-      export $(undash ${arg})
-    else
-      echo "${arg}" |  grep '^\-\-[a-z_]\+$' > /dev/null
-      if [ $? -eq 0 ]; then
-        export $(undash ${arg}=1)
-      else
-        echo "error: could not eval argument '${arg}'. exiting..."
-        exit 1
-      fi
-    fi
+# parse an argument into variables
+#
+# works on arguments of the form
+#   assignments: -[-]var=value [value [...]]
+#   flags:       -[-]flag
+# flags are assigned the value 1
+# assignments are evaluated
+
+parse-argument() {
+  var="$(echo ${1} | sed 's/^\(-*\)\([^=-]*\)\(=*.*\)$/\2/')"
+  if [ -z "$var" ]; then
+    exit 1
+  fi
+
+  # if arg contains an =-token, we export arg=value
+  if [[ "${1}" == *=* ]]; then
+    vals="$(echo ${1} | sed 's/\(^-*\)\([^=-]*\)\(=\)\(.*\)/\4/')"
+    eval "${var}=''"
+    for val in $vals; do
+      eval "${var}=\$(echo \${${var}} ${val})"
+    done
+  # else we assume it to be a flag and export it with value 1
+  else
+    eval "${var}=1"
+  fi
+  export "${var}"
+}
+
+# parse multiple arguments
+#
+# to be used as 'parse_arguments "$@"'
+
+parse_arguments() {
+  while (($#)); do
+    parse_arg "${1}"
     shift
   done
 }
+# function parse-arguments() {
+#   while (( "$#" )); do
+#     arg="$1"
+#     echo "${arg}" |  grep '^\-\-[a-z_]\+=[a-zA-Z_.0-9\-]\+$' > /dev/null
+#     if [ $? -eq 0 ]; then
+#       export $(undash ${arg})
+#     else
+#       echo "${arg}" |  grep '^\-\-[a-z_]\+$' > /dev/null
+#       if [ $? -eq 0 ]; then
+#         export $(undash ${arg}=1)
+#       else
+#         echo "error: could not eval argument '${arg}'. exiting..."
+#         exit 1
+#       fi
+#     fi
+#     shift
+#   done
+# }
 
 function check-for-absent-arguments() {
   for arg in $required_args; do
@@ -73,4 +111,23 @@ function diff-ts() {
   lhs="$(date '%s' -d ${1})"
   rhs="$(date '%s' -d ${2})"
   echo "$(( lhs - rhs ))"
+}
+
+register-flag() {
+  flag="$(echo ${1} | sed 's/^\(-*\)\([^=-]*\)\(=*.*\)$/\2/')"
+  if [ -z "$flag" ]; then
+    exit 1
+  fi
+  if [ -n $(eval "echo \$${flag}") ]; then
+    eval "${flag}=${2}"
+  fi
+}
+
+register-flags() {
+  default_value="$1"
+  shift
+  while (( $# )); do
+    register_flag "$1" "$default_value"
+    shift
+  done
 }
