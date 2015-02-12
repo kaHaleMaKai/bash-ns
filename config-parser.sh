a3196dab9a6748673dea17e7ec86c3276f28d24a
Lars Winderling <lwinderling@performance-advertising.de>
2015-02-12 18:15:49 +0100

    problems with get-in and dissoc-in if using dissoc(-in) also delete array-valued elements

---
+++ config-parser.sh
@@ -0,0 +1,60 @@
+source extended-builtins.sh
+declare -a __CONFIG_SECTIONS__
+declare -A __CONFIG__
+
+private; function get-awk-flavor() {
+  flavor="$(awk -W version 2>/dev/null |
+              head -n 1)"
+  case "$flavor" in
+    *[Gg][Nn][Uu]* | *gawk*)identifier='g'
+      ;;
+    *mawk*) identifier='m'
+      ;;
+    *nawk*) identifier='n'
+      ;;
+    *) identifier='a'
+      ;;
+  esac
+  echo $identifier
+}
+
+function parse-config() {
+  awk \
+    'BEGIN { sections_found = 0;
+             was_error = 0;
+             faulty_line = "";
+             faulty_line_nr = -1;
+             len = 0;
+             cur_section = ""; }
+    /^[#;]|^[ \t]*$/ {
+        next; }
+    /^\[[a-z][a-zA-Z0-9_-]*\][ \t]*/ {
+        gsub(/[\[\] \t]/, "", $0);
+          cur_section = $0;
+          sections[++sections_found] = $0;
+          next; }
+    /^[a-zA-Z_0-9-][a-zA-Z_0-9-]*[ \t]*=[ \t]*[^ \t][^ \t]*/ {
+        if (sections_found == 0) {
+          next; }
+        eq_token_idx = index($0, "=");
+        var = substr($0, 1, eq_token_idx - 2);
+        val = substr($0, eq_token_idx + 1);
+        sub(/[ \t][ \t]*/, "", var);
+        sub(/^[ \t][ \t]*|[ \t][ \t]*$/, "", val);
+        if (tolower(val) == "on") {
+          args[++len] = cur_section" "var; }
+        else if (tolower(val) != "off") {
+          args[++len] = cur_section" "var" "val; }
+        next; }
+    { was_error = 1;
+      faulty_line = $0;
+      faulty_line_nr = NR;
+      exit 1; }
+
+    END { if (was_error == 1) {
+          print "[ERROR] in config file, line "faulty_line_nr > "/dev/stderr";
+          print "\""faulty_line"\"" > "/dev/stderr"; }
+          else {
+            for (i = 1; i <= len; ++i) {
+              print args[i]; } } }'
+}
